f8fc0d040ec0ed9f8c146d8506c02365e5277509
Daniel Sapkota <daniel.sapkota15@my.northampton.ac.uk>
2018-05-20 23:53:41 -0500

    Fixed bug while logging in

--- script.py
+++ script.py
@@ -39,8 +39,8 @@ def load_facebook():
     username = input("Enter Facebook Username:")
     pswd = getpass.getpass('Enter Facebook Password:')
     
-    driver.find_element_by_id('email').send_keys('email')
-    driver.find_element_by_id('pass').send_keys('password')
+    driver.find_element_by_id('email').send_keys(username)
+    driver.find_element_by_id('pass').send_keys(pswd)
     driver.find_element_by_id('pass').send_keys(Keys.ENTER)
     
     try:
